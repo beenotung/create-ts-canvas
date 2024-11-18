67e9b7be4c6843bc006025d1ff78ff676bd24d7c
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-11-19 01:38:04 +0800

    feat: use esbuild instead of tsc to support bundling dependencies

--- template/main.ts
+++ template/main.ts
@@ -1,3 +1,5 @@
+import { new_oklab, new_rgb, oklab_to_rgb, range } from 'oklab.ts/dist/oklab'
+
 let canvas = document.createElement('canvas')
 
 let ratio = 10
@@ -13,14 +15,26 @@ canvas.style.height = canvas.height * ratio + 'px'
 let context = canvas.getContext('2d')!
 let imageData = context.getImageData(0, 0, canvas.width, canvas.height)
 
+let rgb = new_rgb()
+let oklab = new_oklab()
+
 function tick() {
+  let t = Math.sin(Date.now() / 1000)
+
   for (let i = 0; i < batch; i++) {
     let x = Math.floor(Math.random() * canvas.width)
     let y = Math.floor(Math.random() * canvas.height)
     let offset = (y * canvas.width + x) * 4
-    imageData.data[offset + 0] = 132
-    imageData.data[offset + 1] = 64
-    imageData.data[offset + 2] = 132
+
+    oklab.L = ((t + 1) / 2) * range.L.range + range.L.min
+    oklab.a = (x / canvas.width) * range.a.range + range.a.min
+    oklab.b = (y / canvas.height) * range.b.range + range.b.min
+
+    oklab_to_rgb(oklab, rgb)
+
+    imageData.data[offset + 0] = rgb.r
+    imageData.data[offset + 1] = rgb.g
+    imageData.data[offset + 2] = rgb.b
     imageData.data[offset + 3] = 255
   }
 
